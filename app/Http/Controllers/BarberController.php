f2f94c8d53d27ecd8bbb012d94f8fc5d35d8d7b7
reginaldo2307 <reginaldo1982@hotmail.com>
2022-07-30 17:56:05 -0300

    Alteracao buscas

--- app/Http/Controllers/BarberController.php
+++ app/Http/Controllers/BarberController.php
@@ -380,4 +380,27 @@ class BarberController extends Controller
         }
         return $array;
     }
+
+    public function search(Request $request) {
+        $array = ['error'=>'', 'list'=>[]];
+
+        $q = $request->input('q');
+
+        if($q) {
+
+            $barbers = Barber::select()
+                ->where('name', 'LIKE', '%'.$q.'%')
+            ->get();
+
+            foreach($barbers as $bkey => $barber) {
+                $barbers[$bkey]['avatar'] = url('media/avatars/'.$barbers[$bkey]['avatar']);
+            }
+
+            $array['list'] = $barbers;
+        } else {
+            $array['error'] = 'Digite algo para buscar';
+        }
+
+        return $array;
+    }
 }
